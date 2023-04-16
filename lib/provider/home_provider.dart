bfbed8a22edf6b5de4ba6d76bcc740f43a629fdf
oswaldo olea <olea.oswaldo@gmail.com>
2023-04-16 18:45:17 +0200

    second commit

---
+++ lib/provider/home_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_http_rest_api/model/home_model.dart';
+import 'package:flutter_http_rest_api/repository/home_repo.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomeProvider with ChangeNotifier {
+  HomeModel? model;
+
+  bool isLoaded = false;
+
+  Future<void> getCountryApi() async {
+    model = await HomeRepo().getCountryData();
+    setIsLoaded(true);
+    notifyListeners();
+  }
+
+  bool getIsLoaded() {
+    return isLoaded;
+  }
+
+  void setIsLoaded(bool value) {
+    isLoaded = value;
+    notifyListeners();
+  }
+}
